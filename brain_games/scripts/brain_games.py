05573b65eadbf8127b233565016ce974a8577d27
SergeKapinus <kapinus.serg@gmail.com>
2023-06-13 10:29:14 +0300

    step 2, add cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
 
 def greet_hello():
@@ -7,6 +8,7 @@ def greet_hello():
 
 def main():
 	greet_hello()
+	welcome_user()
 
 
 if __name__ == '__main__':

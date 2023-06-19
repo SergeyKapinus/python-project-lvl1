5400f3b929942a4d2d1cbce04bf5dd610a832d39
Капинус Сергей Валерьевич <SeVKapinus@atex.local>
2023-06-19 12:41:50 +0300

    Step 5

--- brain_games/cli_even.py
+++ brain_games/cli_even.py
@@ -26,21 +26,4 @@ def get_answer():
     return prompt.string('Yuor answer: ')
 
 
-def play_even():
-    greet_hello()
-    name = get_name()
-    print(f'Hello, {name}!')
-    get_discription()
-    i = 1
-    while i <= 3:
-        number = get_integer()
-        print(f'Question: {number}')
-        answer = get_answer()
-        if is_even(number) and answer == 'yes' or not is_even(number) and answer == 'no':
-            print('Correct!')
-        elif is_even(number) and answer == 'no':
-            return print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
-        else:
-            return print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
-        i += 1
-    return print(f'Congratulations, {name}!')
+
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 
 
-from brain_games.cli_even import play_even
+from brain_games.cli_even import greet_hello, get_name, get_discription
+from brain_games.cli_even import is_even, get_integer, get_answer
 
+
+def play_even():
+    greet_hello()
+    name = get_name()
+    print(f'Hello, {name}!')
+    get_discription()
+    i = 1
+    while i <= 3:
+        number = get_integer()
+        print(f'Question: {number}')
+        answer = get_answer()
+        if is_even(number) and answer == 'yes' or not is_even(number) and answer == 'no':
+            print('Correct!')
+        elif is_even(number) and answer == 'no':
+            return print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
+        else:
+            return print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+        i += 1
+    return print(f'Congratulations, {name}!')
 
 def main():
     play_even()
